9df1a83ac629c690b52f09a6f28fbdd78fdba408
Brian DAmore <brian.damore@gmail.com>
2015-08-10 13:24:23 -0600

    twilio added

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 ////////////DEPENDENCIES///////////////////
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
+var twilio = require('twilio');
+
 
 var server = express();
 var port = 8080;
+
+//Middleware///
+server.use(express.static('public'));
 
 server.listen(port, function (){
     console.log("I'm still watching you", port)
 });
 
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 server.use(bodyParser.json());
+server.use(cors());
 
 var messages = [{message: 'Fuck you'}];
 
-server.get('/', function(req, res) {
-    res.send(JSON.stringify(messages));
+server.get('/api/testData/:attitude', function(req, res) {
+    console.log("req.params: ", req.params)
+    console
+    return res.json((messages));
 });
 
-server.post('/', function(req, res){
-    var newMessage = req.body.message;
+server.post('/api/receive_message', function(req, res){
+    console.log(req.body);
+    var newMessage = req.body.message
     messages.push(newMessage);
-    res.end("it worked");
+    res.json((messages));
 });
 
 
